c5eecbdd0b88e2ec3413f4958dcab3c704c0638e
NERFOO <james.jaime.1105@gmail.com>
2023-05-23 10:26:36 +0200

    Add project files.

---
+++ 04_MvcAWSPostgresEC2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _04_MvcAWSPostgresEC2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
